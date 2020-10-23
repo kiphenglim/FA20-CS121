857a7555c327d8050737f4845429ff2b6ff4dac8
Vivian Pou <51673732+vivpou@users.noreply.github.com>
2020-10-22 20:39:45 -0400

    add functions for genre predictions and probabilities

--- front.py
+++ front.py
@@ -37,32 +37,61 @@ def uploadFile():
       filename = secure_filename(file.filename)
       fullPath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
       file.save(fullPath)
-      return render_template("index.html", uploadedImagePath = fullPath, stylePrediction=predictStyleCategory(file), styleProbability=predictStyleProb(file))
+      return render_template("index.html", uploadedImagePath = fullPath, stylePrediction=predictStyleCategory(file), styleProbability=predictStyleProb(file), genrePrediction=predictGenreCategory(file), genreProbability=predictGenreProb(file))
     else:
       flash('Please select a file with a .png, .jpg, .jpeg, or .gif extension')
       
   return render_template("index.html",uploadedImagePath = os.path.join('static', "uploadPH.jpg"))
 
+### STYLE ###
 # load the learner
-learn = load_learner(path='./models', file='allStylesBasic.pkl')
-classes = learn.data.classes
+styleLearn = load_learner(path='./models', file='allStylesBasic.pkl')
+styleClasses = styleLearn.data.classes
 styleList = ['Abstract Expressionism', 'Action Painting', 'Analytical Cubism', 'Art Nouveau', 'Baroque', 'Color Field Painting', 'Contemporary Realism', 'Cubism', 'Early_Renaissance', 'Expressionism', 'Fauvism', 'High Renaissance', 'Impressionism', 'Mannerism Late Renaissance', 'Minimalism', 'Naive Art Primitivism', 'New Realism', 'Northern Renaissance', 'Pointillism', 'Pop Art', 'Post Impressionism', 'Realism', 'Rococo', 'Romanticism', 'Symbolism', 'Synthetic Cubism', 'Ukiyo e']
 
 # make prediction and load into json
 def predictStyleCategory(img_file):
   # function to take image and return prediction
-  prediction = learn.predict(open_image(img_file))
+  prediction = styleLearn.predict(open_image(img_file))
   probs_list = prediction[2].numpy()
-  predictionKey = int(classes[prediction[1].item()])
+  predictionKey = int(styleClasses[prediction[1].item()])
   return "Predicted Category: " + styleList[predictionKey]
 
 def predictStyleProb(img_file):
   # function to take in image and return top 5 predictions
-  prediction = learn.predict(open_image(img_file))
+  prediction = styleLearn.predict(open_image(img_file))
   probs_list = prediction[2].numpy()
-  probabilityRaw = {styleList[c]: round(float(probs_list[i]), 5) for (i, c) in enumerate(classes)}
+  probabilityRaw = {styleList[c]: round(float(probs_list[i]), 5) for (i, c) in enumerate(styleClasses)}
   
-  print(type(probabilityRaw))
+  probabilitySorted = sorted(probabilityRaw.items(), key=lambda x: x[1], reverse=True)
+
+  topFiveProb = str(probabilitySorted[:5])
+  specialChars = ['[', ']', "'"]
+  for i in specialChars:
+    topFiveProb = topFiveProb.replace(i, "")
+  
+  return "Top 5 Probabilities: " + topFiveProb
+
+
+### GENRE ###
+# load the learner
+genreLearn = load_learner(path='./models', file='allGenreBasic.pkl')
+genreClasses = genreLearn.data.classes
+
+# make prediction and load into json
+def predictGenreCategory(img_file):
+  # function to take image and return prediction
+  prediction = genreLearn.predict(open_image(img_file))
+  probs_list = prediction[2].numpy()
+  predictionKey = genreClasses[prediction[1].item()]
+  return "Predicted Category: " + str(predictionKey)
+
+def predictGenreProb(img_file):
+  # function to take in image and return top 5 predictions
+  prediction = genreLearn.predict(open_image(img_file))
+  probs_list = prediction[2].numpy()
+  probabilityRaw = {c: round(float(probs_list[i]), 5) for (i, c) in enumerate(genreClasses)}
+  
   probabilitySorted = sorted(probabilityRaw.items(), key=lambda x: x[1], reverse=True)
 
   topFiveProb = str(probabilitySorted[:5])
