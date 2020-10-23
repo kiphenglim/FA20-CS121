e056eaa577eb955ded8039367e58b1e65f1a260d
Vivian Pou <51673732+vivpou@users.noreply.github.com>
2020-10-23 00:16:36 -0400

    further attemps for POST instead of GET

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,12 +1,12 @@
 function postData(){
     data = {
-        file: document.getElementById("file"),
-        style: document.getElementById("style"),
-        genre: document.getElementById("genre"),
-        artist: document.getElementById("artist")
+        "file": document.getElementById("file"),
+        "style": document.getElementById("style"),
+        "genre": document.getElementById("genre"),
+        "artist": document.getElementById("artist")
     }
     var xhr = new XMLHttpRequest();
-    xhr.open("POST", '/', true);
+    xhr.open("POST", '', true);
     xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.send(JSON.stringify(data.json()));
+    xhr.send(JSON.stringify({}));
 }
