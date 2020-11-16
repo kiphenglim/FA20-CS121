b67d4237564f1ebef51ab1a9939b5f673bfb433d
kiphenglim <klim@hmc.edu>
2020-11-16 01:31:28 -0800

    perfect image display and removal
    We had two different problems: 1. When the user first uploaded an
    image, there was no way for them to see it before running the models,
    and 2. When the user clicked on "Reset", the image wasn't cleared from
    the screen, potentially leading them to believe it was still
    available. Both of these problems are fixed with a bit of JS magic.
    
    This functionality has been tested on Chrome, Edge, and Firefox. This
    should fix Issue #32, 33.

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,7 +1,21 @@
+function updateImage(input) {
+    if (input.files && input.files[0]) {
+	var reader = new FileReader();
+
+	reader.onload = function (e) {
+	    document.getElementById("uploaded_image").src = e.target.result;
+	};
+
+	reader.readAsDataURL(input.files[0]);
+    }
+    document.getElementById("options").scrollIntoView();
+}
+
 function updateBoxes(boxId, box2Id) {
     document.getElementById(box2Id).checked = document.getElementById(boxId).checked;
 }
 
 function refresh() {
+    document.getElementById("uploaded_image").src = "static/uploadPH.jpg";
     document.getElementById("options").scrollIntoView();
 }
